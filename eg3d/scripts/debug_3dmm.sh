efccb8dbd23aedf2fe8e38310ff29ef2d0918deb
mengxuyiGit <mengxuyiabc@gmail.com>
2023-10-09 13:13:07 +0000

    added debug script to run triplane_next3d.synthesis()

---
+++ eg3d/scripts/debug_3dmm.sh
@@ -0,0 +1,4 @@
+# Train with FFHQ from scratch with raw neural rendering resolution=64, using 8 GPUs.
+python3 train_debug.py --outdir=~/training-runs --cfg=ffhq --data=/mnt/kostas-graid/datasets/xuyimeng/ffhq/FFHQ_512.zip \
+  --gpus=1 --batch=4 --gamma=1 --gen_pose_cond=True  
+
