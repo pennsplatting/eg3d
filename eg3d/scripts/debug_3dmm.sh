9798fe67cc953c2243f43af74c72125fe52d67a3
mengxuyiGit <mengxuyiabc@gmail.com>
2024-01-15 05:49:28 +0000

    training script using 1k white bg data

--- eg3d/scripts/debug_3dmm.sh
+++ eg3d/scripts/debug_3dmm.sh
@@ -1,5 +1,5 @@
 # Train with FFHQ from scratch with raw neural rendering resolution=64, using 8 GPUs.
-python3 eg3d/train_debug.py --outdir=~v2/training-runs --cfg=ffhq --data=/home/xuyimeng/Repo/eg3d/FFHQ_subset_6_finished.zip \
+python3 eg3d/train_debug.py --outdir=~v2/training-runs --cfg=ffhq --data=/home/xuyimeng/Repo/eg3d/FFHQ_subset_1k_white_bg_finished.zip \
   --gpus=1 --batch=4 --gamma=1 --gen_pose_cond=True --snap=5
 
 # python3 eg3d/train_debug.py --outdir=~v2/training-runs --cfg=ffhq --data=/home/xuyimeng/Data/FFHQ_png_512.zip \
